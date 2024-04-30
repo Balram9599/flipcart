53c587bb7de3f02b77353a50844a53e2ba6a3110
Balram9599 <balramgupta9599@gmail.com>
2024-04-30 22:11:52 +0530

    first commit

---
+++ fc.js
@@ -0,0 +1,36 @@
+
+// let slides = document.querySelectorAll(".slide");
+// let counter = 0;
+// slides.forEach((slide, index)=>{
+//     slide.style.left = `${index *100}%`;
+
+// })
+
+// function goPre(){
+//     counter++;
+//     slideimg();
+// }
+// function goNext(){
+//     // counter--;
+//     slideimg();
+    
+// }
+
+
+// function slideimg (){
+//     slides.forEach(
+//         (slide)=>{
+//            slide.style.transform = `translateX(-${counter*100}%)`; 
+//         }
+//     )
+// }
+
+// async function slideimage(slide, delay){
+//     return new Promise((resolve, reject)=>{
+//       setTimeout(()=>{
+//      slideimg();
+//       },delay)
+//     });
+// }
+
+// slideimage(`translateX(-${counter*100}%)`,1000);
